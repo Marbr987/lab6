23da0030102530237e268f8c3ac05978b2d725aa
dharaniganesandd <dharanikarshygan004@gmail.com>
2021-10-01 13:01:31 +0200

    check it out

--- R/brute_force_knapsack.R
+++ R/brute_force_knapsack.R
@@ -1,3 +1,10 @@
+#' brute_force_knapsack
+#' @description An algorithm that tries every possible alternatives of items in the knapsack and returns the optimal solution.
+#' @param x dataframe with columns w and v (weight and value) representing the items
+#' @param W double representing the weight limit of the knapsack
+#' @return list with the value of the items in the knapsack and the elements contained in the knapsack.
+#' @export
+
 brute_force_knapsack <- function(x, W){
 some_number = c()  
 some_element1=c()
