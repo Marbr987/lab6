3aa9838497b673263df66d6a7b9730db9c4cc556
RajaHussnain <hussnainkhalid92@gmail.com>
2021-10-02 03:59:43 +0200

    Hussnain 1.1.1 1: greedy_knapsack updated.

--- R/greedy_knapsack.R
+++ R/greedy_knapsack.R
@@ -8,8 +8,30 @@
 greedy_knapsack <- function(x, W){
   # Check if input x is correct
   if(names(x) != c("w", "v") || ncol(x) != 2 || typeof(x) != "list"){stop("x must be a dataframe with column names w and v")}
-  # some code
-  # 
-  result <- list(value="some number", elements=c("some element1", "some element2"))
+  
+  perm = order(x$v/x$w, decreasing = TRUE)
+  x <- x[perm,]
+  i <- 1
+  sum <- 0
+  while(sum <= W) {
+    sum <- sum(x$w[1:i])
+    i = i + 1
+  }
+  total_value <- sum(x$v[1:(i-2)])
+  elements <- as.numeric(rownames(x[1:(i-2),]))
+  
+  result <- list(value = round(total_value), elements = elements)
   return(result)
 }
+
+RNGversion(min(as.character(getRversion()),"3.5.3"))
+set.seed(42, kind = "Mersenne-Twister", normal.kind = "Inversion")
+n <- 2000
+knapsack_objects <-
+  data.frame(
+    w=sample(1:4000, size = n, replace = TRUE),
+    v=runif(n = n, 0, 10000)
+  )
+
+greedy_knapsack(x = knapsack_objects[1:800,], W = 3500)
+greedy_knapsack(x = knapsack_objects[1:1200,], W = 2000)
