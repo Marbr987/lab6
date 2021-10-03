f90a1ba0cfa0480efb10887c1bfcaa0434c9f752
Marc Braun <marc.y.braun@live.de>
2021-10-03 13:57:42 +0200

    correct output of brute force

--- R/brute_force_knapsack.R
+++ R/brute_force_knapsack.R
@@ -6,9 +6,8 @@
 #' @export
 
 brute_force_knapsack <- function(x, W){
-some_number = c()  
-some_element1=c()
-some_element2= c()
+value = c()  
+elements=c()
 weight=c()
 value=c()
 #Going through rows until we reach the end.
@@ -24,32 +23,19 @@ weight = which(sum_w<= W)
 if(length(weight) != 0)
 { 
 value = sum_v[weight]
-some_number =max(value)
-A= which((value) == some_number)
+value =max(value)
+A= which((value) == value)
 maxValWghtIdx =weight[A]
 maxValWght = w[,maxValWghtIdx]
 #add number of chosen items with max value to some_element 1.
 j = 1
 while (j<=i){
-some_element1[j] =which(x[,1] == maxValWght[j])
+elements[j] =which(x[,1] == maxValWght[j])
 j=j+1
 }
 }
 i=i+1
 }
-return(list(value = round(some_number), elements = some_element1 , some_element2))
+return(list(value = round(value), elements = elements))
 return(result)
 }
-
-
-##Print
-{
-set.seed(42, kind = "Mersenne-Twister", normal.kind = "Inversion")
-n <- 2000
-knapsack_objects <-
-  data.frame(
-    w=sample(1:4000, size = n, replace = TRUE),
-    v=runif(n = n, 0, 10000))
-}
-brute_force_knapsack(x = knapsack_objects[1:12,], W = 2000)
-brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500)
